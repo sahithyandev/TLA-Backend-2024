8a6648ef7ee21709700602919dec8972e276afc1
Kukesan <kkukesan@gmail.com>
2024-03-04 01:17:07 +0530

    swagger removed

--- api/models/contactModel.js
+++ api/models/contactModel.js
@@ -1,65 +1,8 @@
 "use strict";
-
-/**
- * @swagger
- * components:
- *   schemas:
- *     Contact:
- *       type: object
- *       properties:
- *         id:
- *           type: integer
- *         name:
- *           type: string
- *         email:
- *           type: string
- *         message:
- *           type: string
- *         phoneNumber:
- *           type: string
- *         category:
- *           type: string
- */
-// const { DataTypes } = require("sequelize");
-// const sequelize = require("../../db/db");
-
-// const Contact = sequelize.define("Contact", {
-//   id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true,
-//     autoIncrement: true,
-//   },
-//   name: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   email: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//     validate: {
-//       isEmail: true,
-//     },
-//   },
-//   message: {
-//     type: DataTypes.STRING,
-//     allowNull: true, // message can be optional
-//   },
-//   phoneNumber: {
-//     type: DataTypes.STRING,
-//     allowNull: true, // phoneNumber can be optional
-//   },
-//   category: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-// });
-
-// module.exports = Contact;
-
 const mongoose = require("mongoose");
 
 const contactSchema = new mongoose.Schema({
-  name: {
+  userName: {
     type: String,
     required: true,
   },
--- db/db.js
+++ db/db.js
@@ -1,20 +1,3 @@
-// "use strict";
-// const { Sequelize } = require("sequelize");
-// const sql = require("../config/config").sql;
-
-// const sequelize = new Sequelize(sql.database, sql.user, sql.password, {
-//   host: sql.server,
-//   port: sql.port,
-//   dialect: sql.dialect,
-//   dialectOptions: {
-//     encrypt: sql.options.encrypt,
-//     enableArithAbort: sql.options.enableArithAbort,
-//   },
-//   pool: sql.pool,
-// });
-
-// module.exports = sequelize;
-
 "use strict";
 const mongoose = require("mongoose");
 const { mongodbUri } = require("../config/config");
